b9a8aafdf00eeaba57e572f76f58f06cc2da9aab
guardianprime <okongordian@gmail.com>
2025-10-07 05:08:25 +0100

    game utils file and test code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import LandingPage from "./pages/LandingPage";
 import { io } from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 //import Lobby from "./pages/Lobby";
 
 function App() {
+  const [newGameId, setNewGameId] = useState(null);
+
   useEffect(() => {
     const socket = io("http://localhost:5000");
     socket.on("connect", () => {
@@ -16,8 +18,8 @@ function App() {
 
   return (
     <>
-      <LandingPage />
-      {/*  <Lobby /> */}
+      <LandingPage setNewGameId={setNewGameId} />
+      {newGameId && <Lobby />}
     </>
   );
 }
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,30 @@
-function LandingPage() {
+function LandingPage({ setNewGameId }) {
+  function handleCreateGame() {
+    console.log("Create game button clicked");
+    setNewGameId("12345"); // Example game ID
+  }
+
+  function handleJoinGame(e) {
+    e.preventDefault();
+    console.log("Join game button clicked");
+  }
+
   return (
     <div>
       <h1>This is a guessing game</h1>
       <p>join a game and guess the answer!</p>
-      <input type="text" />
-      <button>join game</button>
+      <form onSubmit={(e) => handleJoinGame(e)}>
+        <input type="text" />
+        <input type="submit" value="join game" />
+      </form>
       <p>or create your own game</p>
-      <button>Create a game</button>
+      <button
+        onClick={() => {
+          handleCreateGame();
+        }}
+      >
+        Create a game
+      </button>
     </div>
   );
 }
